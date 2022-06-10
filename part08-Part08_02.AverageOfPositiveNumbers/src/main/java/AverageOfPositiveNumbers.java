f7e21a33449d4dc791b4a67f555f3c725ddcb608
Pablo <pablosgrad12@gmail.com>
2022-06-09 22:27:36 -0600

    Tests passed for Average of positive program

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -4,7 +4,25 @@ import java.util.Scanner;
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
+        double sum = 0;
+        int num, count = 0;
         Scanner scanner = new Scanner(System.in);
-
+        
+        while(true){
+            num = scanner.nextInt();
+            
+            if(num == 0){
+                break;
+            }else if(num > 0){
+                sum += num;
+                count++;
+            }
+        }
+        //sum being 0 means no positive numbers were inputted.
+        if(sum == 0){
+            System.out.println("Cannot calculate the average");
+        }else{
+            System.out.println(sum / count);
+        }
     }
 }
