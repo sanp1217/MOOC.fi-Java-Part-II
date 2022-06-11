da45ef4ba0650c8150d0ddac4edd4a467fe15024
Pablo <pablosgrad12@gmail.com>
2022-06-10 21:38:43 -0600

    Adding command added

--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -4,17 +4,33 @@ import java.util.Scanner;
 public class LiquidContainers {
 
     public static void main(String[] args) {
+        int containerOne = 0, containerTwo = 0;
         Scanner scan = new Scanner(System.in);
 
 
         while (true) {
+            System.out.println("First: " + containerOne + "/" + "100");
+            System.out.println("Second: " + containerTwo + "/" + "100");
+            
             System.out.print("> ");
-
             String input = scan.nextLine();
+            
             if (input.equals("quit")) {
                 break;
             }
-
+            
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            
+            if(command.equals("add")){
+                if(amount > 0){
+                    containerOne += amount;
+                }
+                if(containerOne > 100){
+                    containerOne = 100;
+                }
+            }
         }
     }
 
