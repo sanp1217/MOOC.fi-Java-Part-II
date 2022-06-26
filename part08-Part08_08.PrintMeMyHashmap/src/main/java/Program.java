6073813421ec5a798e3a5060297cdf958b6e9e8c
Pablo <pablosgrad12@gmail.com>
2022-06-26 17:35:15 -0600

    Makes printKeys method

--- part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -1,9 +1,26 @@
-
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Program {
-
+    
     public static void main(String[] args) {
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("f.e", "for example");
+        hashmap.put("etc.", "and so on");
+        hashmap.put("i.e", "more precisely");
+
+        printKeys(hashmap);
+    }
+
+    public static void printKeys(HashMap<String, String> hashmap){
+        ArrayList<String> keys = new ArrayList<>();
         
+        for(String key: hashmap.keySet()){
+            keys.add(key);
+        }
+        
+        for(int i = 0; i < keys.size(); i++){
+            System.out.println(keys.get(i));
+        }
     }
 }
