aa8f36165b71791e847945df1dec9ab20cf96315
Pablo <pablosgrad12@gmail.com>
2022-06-26 17:54:33 -0600

    Refactors methods

--- part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -17,46 +17,24 @@ public class Program {
     }
 
     public static void printKeys(HashMap<String, String> hashmap){
-        ArrayList<String> keys = new ArrayList<>();
-        
         for(String key: hashmap.keySet()){
-            keys.add(key);
-        }
-        
-        for(int i = 0; i < keys.size(); i++){
-            System.out.println(keys.get(i));
-        }
-    }
-    
-    public static void printKeysWhere(HashMap<String, String> hashmap, String text){
-        ArrayList<String> keys = new ArrayList<>();
-        
-        for(String key: hashmap.keySet()){
-            if(!key.contains(text)){
-                continue;
-            }
-            
-            keys.add(key);
-        }
-        
-        for(String key: keys){
             System.out.println(key);
         }
     }
     
-    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text){
-        ArrayList<String> values = new ArrayList<>();
-        
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text){
         for(String key: hashmap.keySet()){
-            if(!key.contains(text)){
-                continue;
+            if(key.contains(text)){
+                System.out.println(key);
             }
-            
-            values.add(hashmap.get(key));
         }
-        
-        for(String value: values){
-            System.out.println(value);
+    }
+    
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text){
+        for(String key: hashmap.keySet()){
+            if(key.contains(text)){
+                System.out.println(hashmap.get(key));
+            }
         }
     }
     
