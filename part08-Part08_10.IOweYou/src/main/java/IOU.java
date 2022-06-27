141f9ee75b2b7565678b8c6fcab2825bb1f7d94a
Pablo <pablosgrad12@gmail.com>
2022-06-27 13:36:41 -0600

    Adds IOU class, all tests passed

--- part08-Part08_10.IOweYou/src/main/java/IOU.java
+++ part08-Part08_10.IOweYou/src/main/java/IOU.java
@@ -1,13 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.HashMap;
 
-/**
- *
- * @author choco
- */
 public class IOU {
+    private HashMap<String, Double> debts;
     
+    public IOU(){
+        this.debts = new HashMap<>();
+    }
+    
+    public void setSum(String toWhom, double amount){
+        this.debts.put(toWhom, amount);
+    }
+    
+    public double howMuchDoIOweTo(String toWhom){
+        return this.debts.getOrDefault(toWhom, 0.0);
+    }
 }
