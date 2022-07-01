fe8c8f907c962fc10bcdd775e710b1f793695a67
Pablo <pablosgrad12@gmail.com>
2022-06-30 20:49:07 -0600

    Adds add, get, and remove methods

--- part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
+++ part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
@@ -1,3 +1,30 @@
+import java.util.HashMap;
+
 public class VehicleRegistry {
+    private HashMap<LicensePlate, String> owners;
     
+    public VehicleRegistry(){
+        owners = new HashMap<>();
+    }
+    
+    public boolean add(LicensePlate licensePlate, String owner){
+        if(owners.get(licensePlate) == null){
+            owners.put(licensePlate, owner);
+            return true;
+        }
+        return false;
+    }
+    
+    public String get(LicensePlate licensePlate){
+        return owners.get(licensePlate);
+    }
+    
+    public boolean remove(LicensePlate licensePlate){
+        if(owners.get(licensePlate) != null){
+            owners.remove(licensePlate);
+            return true;
+        }
+        
+        return false;
+    }
 }
