af9c035d0f639d3705fe616e61834239c3d2ebb7
Pablo <pablosgrad12@gmail.com>
2022-07-02 14:21:15 -0600

    Adds printPersons method.

--- part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -1,20 +1,20 @@
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-       Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
-System.out.println(ada);
-System.out.println(esko);
+        ArrayList<Person> persons = new ArrayList<Person>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
 
-Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-
-int i = 0;
-while (i < 25) {
-  ollie.study();
-  i = i + 1;
-}
-System.out.println(ollie);
+        printPersons(persons);
+    }
+    
+    public static void printPersons(ArrayList<Person> persons){
+        for(int i = 0; i < persons.size(); i++){
+            System.out.println(persons.get(i).toString());
+        }
     }
 
 }
