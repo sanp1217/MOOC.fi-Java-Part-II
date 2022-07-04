93372c6d15659d5b1842d1dcb596adf99bd20331
Pablo <pablosgrad12@gmail.com>
2022-07-04 16:10:07 -0600

    Adds min, max, and average finding mathods.

--- part09-Part09_03.Warehousing/src/main/java/ChangeHistory.java
+++ part09-Part09_03.Warehousing/src/main/java/ChangeHistory.java
@@ -20,4 +20,50 @@ public class ChangeHistory {
     public String toString(){
         return this.prodHistory.toString();
     }
+    
+    public double maxValue(){
+        if(this.prodHistory.isEmpty()){
+            return 0.0;
+        }else{
+            double max = this.prodHistory.get(0);
+            
+            for(int i = 1; i < this.prodHistory.size(); i++){
+                if(this.prodHistory.get(i) > max){
+                    max = this.prodHistory.get(i);
+                }
+            }
+            
+            return max;
+        }
+    }
+    
+    public double minValue(){
+        if(this.prodHistory.isEmpty()){
+            return 0.0;
+        }else{
+            double min = this.prodHistory.get(0);
+            
+            for(int i = 1; i < this.prodHistory.size(); i++){
+                if(this.prodHistory.get(i) < min){
+                    min = this.prodHistory.get(i);
+                }
+            }
+            
+            return min;
+        }
+    }
+    
+    public double average(){
+        double sum = 0;
+        
+        if(this.prodHistory.isEmpty()){
+            return 0.0;
+        }else{
+            for(int i = 0; i < this.prodHistory.size(); i++){
+                sum += this.prodHistory.get(i);
+            }
+        }
+        
+        return sum / this.prodHistory.size();
+    }
 }
