3017a37c058254eeedb850822ed5e01e0c8b34b4
Pablo <pablosgrad12@gmail.com>
2022-07-11 21:50:57 -0600

    Makes TripleTacoBox with methods

---
+++ part09-Part09_05.TacoBoxes/src/main/java/TripleTacoBox.java
@@ -0,0 +1,16 @@
+
+public class TripleTacoBox implements TacoBox{
+    private int tacos;
+    
+    public TripleTacoBox(){
+        this.tacos = 3;
+    }
+    
+    public int tacosRemaining(){
+        return this.tacos;
+    }
+    
+    public void eat(){
+        this.tacos--;
+    }
+}
