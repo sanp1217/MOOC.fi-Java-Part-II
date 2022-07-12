1dfc57afc571c22c4a45f16e5ffc40be292b28c8
Pablo <pablosgrad12@gmail.com>
2022-07-12 12:52:21 -0600

    Adds packable interface and book class

---
+++ part09-Part09_06.InterfaceInABox/src/main/java/Book.java
@@ -0,0 +1,21 @@
+
+public class Book implements Packable{
+    private String author, title;
+    private double weight;
+    
+    public Book(String author, String title, double weight){
+        this.author = author;
+        this.title = title;
+        this.weight = weight;
+    }
+
+    @Override
+    public double weight() {
+        return this.weight;
+    }
+    
+    @Override
+    public String toString(){
+        return this.author + ": " + this.title; 
+    }
+}
--- part09-Part09_06.InterfaceInABox/src/main/java/Main.java
+++ part09-Part09_06.InterfaceInABox/src/main/java/Main.java
@@ -2,7 +2,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        // test your classes here
+        Book book1 = new Book("Fyodor Dostoevsky", "Crime and Punishment", 2);
+        Book book2 = new Book("Robert Martin", "Clean Code", 1);
+        Book book3 = new Book("Kent Beck", "Test Driven Development", 0.5);
+        
+        
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
     }
 
 }
