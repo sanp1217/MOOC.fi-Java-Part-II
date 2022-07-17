ddc278af50849a37b67fb177cedd9e8f6d8e069d
Pablo <pablosgrad12@gmail.com>
2022-07-17 15:03:37 -0600

    Adds item class

---
+++ part09-Part09_10.OnlineShop/src/main/java/Item.java
@@ -0,0 +1,23 @@
+
+public class Item {
+    private String product;
+    private int qty, unitPrice;
+    
+    public Item(String product, int qty, int unitPrice){
+        this.product = product;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+    
+    public int price(){
+        return this.unitPrice * this.qty;
+    }
+    
+    public void increaseQuantity(){
+        this.qty++;
+    }
+    
+    public String toString(){
+        return this.product + ": " + this.qty;
+    }
+}
