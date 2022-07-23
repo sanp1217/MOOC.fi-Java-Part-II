e054d34452f7c5d0ad64f5083c41723167f71323
Pablo <pablosgrad12@gmail.com>
2022-07-22 20:40:01 -0600

    Adds positive method

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -7,8 +8,24 @@ public class PositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // test your method here
+        
+        List<Integer> list = new ArrayList<>();
+        
+        list.add(-9);
+        list.add(8);
+        list.add(-1);
+        
+        System.out.println(positive(list));
+        
 
+    }
+    
+    public static List<Integer> positive(List<Integer> numbers){
+        ArrayList<Integer> positiveValues = numbers.stream()
+                .filter(value -> value > 0)
+                .collect(Collectors.toCollection(ArrayList::new));
+        
+        return positiveValues;
     }
 
 }
