173332561a139e1b28fe629d911b7bacc284ddf1
Pablo <pablosgrad12@gmail.com>
2022-07-23 15:52:21 -0600

    Prints output, passes tests

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -1,11 +1,25 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrintingUserInput {
 
     public static void main(String[] args) {
+        List<String> inputs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        
+        while(true){
+            String input = scanner.nextLine();
+            
+            if(input.isEmpty()){
+                break;
+            }
+            
+            inputs.add(input);
+        }
+        
+        inputs.stream()
+                .forEach(input -> System.out.println(input));
     }
 }
