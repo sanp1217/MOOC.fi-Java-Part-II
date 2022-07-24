0fcf146f51e6b2b51f6edacfdd72350be08cbe06
Pablo <pablosgrad12@gmail.com>
2022-07-23 18:09:29 -0600

    Adds functionality, passes tests

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,27 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LimitedNumbers {
 
     public static void main(String[] args) {
+        List<Integer> inputs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        
+        while(true){
+            int input = scanner.nextInt();
+            
+            if(input < 0){
+                break;
+            }
+            
+            inputs.add(input);
+        }
+        
+        inputs.stream()
+                .filter(num -> num >= 1 && num <= 5)
+                .forEach(num -> System.out.println(num));
+        
     }
 }
