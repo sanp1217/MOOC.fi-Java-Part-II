dc6de28668440883d9ac1c2f14cab27ae631213e
Pablo <pablosgrad12@gmail.com>
2022-07-25 15:10:39 -0600

    Adds method

--- part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -13,5 +13,17 @@ public class ReadingFilesPerLine {
         // test the method here
 
     }
+    
+    public static List<String> read(String file){
+        List<String> rows = new ArrayList<>();
+        
+        try{
+            Files.lines(Paths.get(file)).forEach(row -> rows.add(row));
+        }catch(Exception e){
+            System.out.println("Error");
+        }
+        
+        return rows;
+    }
 
 }
