d54d57d1ddfc71d43ef18f264ca8f338c215b860
Pablo <pablosgrad12@gmail.com>
2022-07-30 22:16:10 -0600

    Adds Book class with methods

---
+++ part10-Part10_14.Literature/src/main/java/Book.java
@@ -0,0 +1,23 @@
+
+public class Book {
+    private String name;
+    private int ageRecommendation;
+    
+    public Book(String name, int ageRecommendation){
+        this.name = name;
+        this.ageRecommendation = ageRecommendation;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public int getAgeRecommendation(){
+        return this.ageRecommendation;
+    }
+    
+    @Override
+    public String toString(){
+        return this.name + " (recommended for " + this.ageRecommendation + "year-olds or older";
+    }
+}
