650138d5d374531413ac5110666c9206c6e364ca
Pablo <pablosgrad12@gmail.com>
2022-07-31 21:56:25 -0600

    Adds day of week checker method

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,12 @@
 
 
 public class Checker {
-
+    
+    
+    public boolean isDayOfWeek(String string){
+        if(string.matches("mon|tue|wed|thu|fri|sat|sun")){
+            return true;
+        }
+        return false;
+    }
 }
