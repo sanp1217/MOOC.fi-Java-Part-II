d3a0045efaa917d18b301089c6d14fe45a83c4fa
Pablo <pablosgrad12@gmail.com>
2022-08-10 15:28:54 -0600

    Completes exercise 6

---
+++ part11-Part11_06.SaveablePerson/src/main/java/Person.java
@@ -0,0 +1,19 @@
+
+public class Person implements Saveable{
+    private String name, address;
+    
+    @Override
+    public void save(){
+        
+    }
+    
+    @Override
+    public void delete(){
+        
+    }
+    
+    @Override
+    public void load(String address){
+        
+    }
+}
