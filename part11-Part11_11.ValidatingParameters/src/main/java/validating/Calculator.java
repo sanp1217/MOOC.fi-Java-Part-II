dfc4f7ebd957081856919e10317be23bdf1f7545
Pablo <pablosgrad12@gmail.com>
2022-08-13 20:01:21 -0600

    Validates calculator

--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
@@ -3,6 +3,9 @@ package validating;
 public class Calculator {
 
     public int factorial(int num) {
+        if(num < 0){
+            throw new IllegalArgumentException("Number can't be negative.");
+        }
 
         int answer = 1;
         for (int i = 1; i <= num; i++) {
@@ -13,6 +16,13 @@ public class Calculator {
     }
 
     public int binomialCoefficent(int setSize, int subsetSize) {
+        if(setSize < 0 || subsetSize < 0){
+            throw new IllegalArgumentException("Parameters can't be negative");
+        }
+        
+        if(subsetSize > setSize){
+            throw new IllegalArgumentException("Subset can't be bigger than set Size");
+        }
 
         int numerator = factorial(setSize);
         int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
