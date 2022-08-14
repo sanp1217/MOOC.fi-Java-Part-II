d631582d95a99f0a9276cad1ddb163a159ffab35
Pablo <pablosgrad12@gmail.com>
2022-08-13 19:54:52 -0600

    Validates person class

--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -5,8 +5,15 @@ public class Person {
     private String name;
     private int age;
 
-    public Person(String name, int age) {
-
+    public Person(String name, int age){
+        if(name == null || name.isEmpty() || name.length() > 40){
+            throw new IllegalArgumentException("Name error");
+        }
+        
+        if(age < 0 || age > 120){
+            throw new IllegalArgumentException("Age error");
+        }
+        
         this.name = name;
         this.age = age;
     }
