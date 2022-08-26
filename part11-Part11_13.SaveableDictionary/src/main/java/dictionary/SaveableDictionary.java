820450f07102fbb6f0cb1550545697056e8031cc
Pablo <pablosgrad12@gmail.com>
2022-08-26 13:38:16 -0600

    Adds extra constructor and load method

--- part11-Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
+++ part11-Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
@@ -1,14 +1,38 @@
 
 package dictionary;
 
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class SaveableDictionary {
     private Map<String, String> translationsMap;
+    private String file;
     
     public SaveableDictionary(){
         this.translationsMap = new HashMap<>();
+    }
+    
+    public SaveableDictionary(String file){
+        this.translationsMap = new HashMap<>();
+        this.file = file;
+    }
+    
+    public boolean load(){
+        try (Scanner scanner = new Scanner(Paths.get(this.file))){
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] parts = line.split(":");
+                
+                add(parts[0], parts[1]);
+            }
+            return true;
+            
+        }catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+            return false;
+        }
     }
     
     public void add(String words, String translation){
