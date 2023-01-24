a68f36b3de6f4cd1aa75d013a9b489084a0c9c51
Pablo <pablosgrad12@gmail.com>
2023-01-23 20:42:09 -0700

    Makes List class with constructor and add method

---
+++ part12-Part12_04.List/src/main/java/List.java
@@ -0,0 +1,16 @@
+public class List<Type> {
+    private Type[] values;
+    private int firstFreeIndex;
+    
+    public List(){
+        this.values = (Type[]) new Object[10];
+        this.firstFreeIndex = 0;
+    }
+    
+    public void add(Type value){
+        this.values[this.firstFreeIndex] = value;
+        this.firstFreeIndex++;
+    }
+    
+    
+}
