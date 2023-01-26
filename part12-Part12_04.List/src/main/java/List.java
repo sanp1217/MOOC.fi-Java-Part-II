b93545f3c440464f2653c7278982d009f91a00a2
Pablo <pablosgrad12@gmail.com>
2023-01-25 20:05:18 -0700

    Implements grow method and modifies add method to use grow

--- part12-Part12_04.List/src/main/java/List.java
+++ part12-Part12_04.List/src/main/java/List.java
@@ -8,8 +8,23 @@ public class List<Type> {
     }
     
     public void add(Type value){
+        if(this.firstFreeIndex == this.values.length){
+            grow();
+        }
+        
         this.values[this.firstFreeIndex] = value;
         this.firstFreeIndex++;
+    }
+    
+    public void grow(){
+        int newSize = this.values.length + this.values.length / 2;
+        Type[] newValues = (Type[]) new Object[newSize];
+        
+        for(int i = 0; i < this.values.length; i++){
+            newValues[i] = this.values[i];
+        }
+        
+        this.values = newValues;
     }
     
     
